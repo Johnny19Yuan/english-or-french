521ebbf8c4bba1b56167a03d4ead038d62bd74fb
Johnny19Yuan <johnny19yuan@gmail.com>
2025-10-06 09:24:29 -0400

    er

--- english or french.py
+++ english or french.py
@@ -1,2 +1,25 @@
+englishorfrench = []
+Ses = 0
+Tes = 0
 englishorfrench = input("english or french")
 
+
+
+for char in englishorfrench:
+    if char == "S":
+        Ses = Ses + 1
+    elif char == "s":
+        Ses = Ses + 1
+    elif char == "T":
+        Tes = Tes + 1
+    elif char == "t":
+        Tes = Tes + 1
+
+if Ses > Tes:
+    print ("It's probally French")
+elif Ses < Tes:
+    print ("It's probally English")
+elif Ses == Tes:
+    print ("Its probally French")
+else:
+    print ("an error")
